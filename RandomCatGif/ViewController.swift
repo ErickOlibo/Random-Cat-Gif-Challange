a9d4811ac55c8febf61f127a407600148ffe9fcd
Erick Olibo <erick@defkut.com>
2019-09-05 10:06:25 +0300

    Initial Commit

---
+++ RandomCatGif/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomCatGif
+//
+//  Created by Erick Olibo on 05/09/2019.
+//  Copyright © 2019 DEFKUT Creations OU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RandomCatGifTests/RandomCatGifTests.swift
@@ -0,0 +1,34 @@
+//
+//  RandomCatGifTests.swift
+//  RandomCatGifTests
+//
+//  Created by Erick Olibo on 05/09/2019.
+//  Copyright © 2019 DEFKUT Creations OU. All rights reserved.
+//
+
+import XCTest
+@testable import RandomCatGif
+
+class RandomCatGifTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
